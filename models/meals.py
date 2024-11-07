cb824fb2ceedeeb83baf6b2950fe8a7ff662dae4
Brunoo67 <bruno.cavalcanti02@proton.me>
2024-11-07 19:42:39 -0300

    Add datetime column

--- models/meals.py
+++ models/meals.py
@@ -1,11 +1,12 @@
 from database import db
 from flask_login import UserMixin
+from datetime import datetime, timezone
 
 class Meal(db.Model, UserMixin):
     __tablename__ = 'meals'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(255))
-    
-    on_diet = db.Column(db.Boolean, nullable=False)
+    description = db.Column(db.String(255), default=" ")
+    on_diet = db.Column(db.Boolean, default=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
